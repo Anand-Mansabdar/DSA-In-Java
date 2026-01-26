33001425d356568d55b92227802c6f79ac936ebc
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-01-26 23:17:21 +0530

    Basics of Java

---
+++ 1_Basics/basics.java
@@ -0,0 +1,33 @@
+public class basics {
+  public static void main(String[] args) {
+    // char ch = '=';
+    // int x = ch;
+    // System.out.println(x);
+
+    // Post Increment
+    int x = 11;
+    System.out.println(x);
+    x++;
+    System.out.println(x);
+
+    // Pre Increment
+    int y = 10;
+    System.out.println(y);
+    --y;
+    System.out.println(y);
+
+    int z = 1;
+    System.out.println(++z);
+    System.out.println(z);
+    System.out.println(z++);
+    System.out.println(z);
+
+    // Same can be done for pre and post decrement
+
+    // RELATONAL OPERATORS
+    System.out.println(45>98);
+    System.out.println(10!=9);
+    System.out.println(1<=100);
+    System.out.println(100==100);
+  }
+}
