533ceacee9b1d3d69673553f71b9bf91749a9aca
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-01-27 23:15:18 +0530

    Check if a number is a 4 digit number or not

---
+++ 2_Conditionals/fourDigitNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class fourDigitNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a number: ");
+    int n = sc.nextInt();
+
+    if(n > 999 && n<10000){
+      System.out.println(+n+" " + "is a 4 digit number");
+    } else {
+      System.out.println(+n+" " + "is not a 4 digit number");
+    }
+  }
+}
