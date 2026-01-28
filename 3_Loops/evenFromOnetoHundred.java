4f856819943b08633a1db2461742409b32731223
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-01-28 21:13:13 +0530

    Basic Loop Questions: print even, print name 'n' times.

---
+++ 3_Loops/evenFromOnetoHundred.java
@@ -0,0 +1,11 @@
+// Print all even numbers from 1-100
+
+public class evenFromOnetoHundred {
+  public static void main(String[] args) {
+    for(int i=1; i<=100; i++){
+      if(i%2 == 0){
+        System.out.println(i);
+      }
+    }
+  }
+}
---
+++ 3_Loops/printOneToTen.java
@@ -0,0 +1,9 @@
+// Loop to print range of numbers
+
+public class printOneToTen {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i);   
+    }
+  }  
+}
