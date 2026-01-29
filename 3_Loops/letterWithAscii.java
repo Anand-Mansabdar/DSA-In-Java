d718a92400b8882daf3a0cd13259923533778836
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-01-29 17:03:22 +0530

    Program to print A-Z along with their ASCII values.

---
+++ 3_Loops/letterWithAscii.java
@@ -0,0 +1,9 @@
+// To print A-Z along with theiwr corresponding ASCII Values...
+
+public class letterWithAscii {
+  public static void main(String[] args) {
+    for(int i=65; i<= 90; i++){
+      System.out.println((char)i + " " + i);
+    }
+  }
+}
