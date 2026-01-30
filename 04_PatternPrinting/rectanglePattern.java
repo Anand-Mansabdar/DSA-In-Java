3fd408642dc230bc3c736210b4c6d3fb26b1456e
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-01-30 22:47:51 +0530

    Program to print a rectangular pattern.

---
+++ 04_PatternPrinting/rectanglePattern.java
@@ -0,0 +1,25 @@
+// To print a rectangular pattern - less rows more columns
+
+// * * * * *
+// * * * * *
+// * * * * *
+
+import java.util.Scanner;
+
+public class rectanglePattern{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the number of rows: ");
+    int rows = sc.nextInt();
+    System.out.println("Enter the number of columns: ");
+    int cols = sc.nextInt();
+
+    for(int i=0; i<rows; i++){
+      for(int j=0; j<cols; j++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
