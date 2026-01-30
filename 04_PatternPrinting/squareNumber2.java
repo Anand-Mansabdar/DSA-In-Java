eb21eb2d567f139c5e1774b7b07a45b077b7e868
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-01-30 23:20:14 +0530

    Program to print "Number Square" variation.

---
+++ 04_PatternPrinting/squareNumber2.java
@@ -0,0 +1,24 @@
+// To print the following pattern - Number Square 2
+
+// 1 1 1 1 
+// 2 2 2 2
+// 3 3 3 3
+// 4 4 4 4
+
+import java.util.Scanner;
+
+public class squareNumber2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the number of rows and columns: ");
+    int n = sc.nextInt();
+
+    for(int i=1; i<=n; i++){
+      for(int j=1; j<=n; j++){
+        System.out.print(i+" ");
+      }
+      System.out.println();
+    }
+  }
+}
