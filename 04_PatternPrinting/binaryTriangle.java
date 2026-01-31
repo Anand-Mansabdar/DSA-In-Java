a91004d5752da2fea53bde7c1b193d59aca8da8f
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-01-31 16:55:01 +0530

    Program to print a binary triangle.

---
+++ 04_PatternPrinting/binaryTriangle.java
@@ -0,0 +1,28 @@
+// To print a binary triangle
+
+// 1 
+// 0 1
+// 1 0 1
+// 0 1 0 1  (for n=4)
+
+import java.util.Scanner;
+
+public class binaryTriangle {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the number of rows: ");
+    int n = sc.nextInt();
+
+    for(int i=1; i<=n; i++){
+      for(int j=1; j<=i; j++){
+        if((i+j)%2 == 0){
+          System.out.print("1 ");
+        } else {
+          System.out.print("0 ");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
