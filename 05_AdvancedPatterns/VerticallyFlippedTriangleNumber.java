867f4c3ca59a1c8eed5778410cf5e8c52df64748
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-02-02 19:01:34 +0530

    Program to print a vertically flipped number triangle.

---
+++ 05_AdvancedPatterns/VerticallyFlippedTriangleNumber.java
@@ -0,0 +1,27 @@
+// To print the following pattern - Verically Flipped Number Triangle
+
+//       1 
+//     1 2 
+//   1 2 3 
+// 1 2 3 4 (for n=4)
+import java.util.Scanner;
+
+public class VerticallyFlippedTriangleNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the number of rows: ");
+    int n = sc.nextInt();
+
+    for(int i=1; i<=n; i++){
+      for(int j=1; j<=n-i; j++){
+        System.out.print("  ");
+      }
+
+      for(int j=1; j<=i; j++){
+        System.out.print(+j+" ");
+      }
+      System.out.println();
+    }
+  }
+}
