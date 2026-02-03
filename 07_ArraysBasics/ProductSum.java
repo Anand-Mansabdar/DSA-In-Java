70c04053b20cc81473dcf12658b23cd62a9fa0c2
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-02-03 21:02:05 +0530

    Program to print the product of elements in array.

---
+++ 07_ArraysBasics/ProductSum.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class ProductSum {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the size of the array: ");
+    int n = sc.nextInt();
+
+    int[] arr = new int[n];
+    System.out.println("Enter elements of the array: ");
+    for(int i=0; i<arr.length; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    int product = 1;
+    for(int i=0; i<arr.length; i++){
+      product *= arr[i];
+    }
+
+    System.out.println("The sum of elements in the array is "+product);
+  }  
+}
