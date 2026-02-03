a95a58219ed18e8230c9f09ab9a1b3697c4b3b5c
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-02-03 17:27:12 +0530

    Function with arguments

---
+++ 06_Methods/FunctionsWithArguments.java
@@ -0,0 +1,8 @@
+public class FunctionsWithArguments {
+  public static int customSumFunction(int a, int b) { // a and b are parameters.
+    return a + b;
+  }
+  public static void main(String[] args) {
+    System.out.println(customSumFunction(10, 20));  // 10 and 20 are called arguments
+  }
+}
