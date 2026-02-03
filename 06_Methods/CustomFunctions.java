f0c09acf3bf677cfe818b06a01c02eb60560abe8
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-02-03 06:30:35 +0530

    Creating custom functions.

---
+++ 06_Methods/CustomFunctions.java
@@ -0,0 +1,15 @@
+public class CustomFunctions {
+  public static void customFunction(){
+    System.out.println("I am a custom function.");
+  }
+
+  public static void anand(){
+    System.out.println("Good morning from Anand Mansabdar.");
+  }
+  public static void main(String[] args) {
+    customFunction();
+    System.out.println("The above function is created by Anand Mansabdar.");
+    customFunction();
+    anand();
+  }
+}
