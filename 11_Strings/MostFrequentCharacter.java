9c7638aed2d0ca3846dccb73b6a0b38f57db9c2f
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-03-03 12:25:14 +0530

    Optimized Approach using Sliding Window

--- 11_Strings/MostFrequentCharacter.java
+++ 11_Strings/MostFrequentCharacter.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MostFrequentCharacter {
   public static char freqCharacter(String s){
     s = s.toLowerCase();
@@ -23,8 +25,39 @@ public class MostFrequentCharacter {
     System.out.println(maxFreq);
     return maxChar;
   }
+
+  public static char optimizedApproach(String s){
+    s=s.toLowerCase();
+    char[] arr = s.toCharArray();
+    int n = s.length();
+    int maxFreq = 0;
+    char ans = s.charAt(0);
+    Arrays.sort(arr);
+
+    int i=0, j=0;
+    while(j<n){
+      if(arr[i] == arr[j]) j++;
+      else {
+        int freq = j-i;
+        if(freq > maxFreq){
+          maxFreq=freq;
+          ans = arr[i];
+        }
+        i=j;
+      }
+    }
+    int freq = j-i;
+    if(freq > maxFreq){
+      maxFreq=freq;
+      ans = arr[i];
+    }
+    System.out.println(maxFreq);
+    return ans;
+  }
   public static void main(String[] args) {
     String s = "Anand Mansabdar";
     System.out.println(freqCharacter(s));
+
+    System.out.println(optimizedApproach(s));
   }
 }
