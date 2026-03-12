32c970edec8e5b11a2f8eb0fe4db733064a7bf16
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-03-12 10:49:27 +0530

    Optimized Approach: Exponentiation by Squaring

--- 14_Recursion/aPowerB.java
+++ 14_Recursion/aPowerB.java
@@ -13,12 +13,26 @@ public class aPowerB {
     int b = sc.nextInt();
 
     System.out.println(power(a, b));
+    System.out.println(optimized(a, b));
   }
 
+  // Higher time complexity O(b)
   public static int power(int a, int b){
     if(a == 0) return 0;
     if(a == 1 || b == 0) return 1;
 
     return a * power(a, b-1);
+  }
+
+  // Optimized Time Complexity O(logn)
+  public static int optimized(int a, int b){
+    if(b == 0) return 1;
+    int call = optimized(a, b/2);
+
+    if(b%2 != 0){
+      return a*call * call;
+    } else {
+      return call * call;
+    }
   }
 }
